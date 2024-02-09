eaf4ef9cead2f6d98eddc32d3e1a11d48f86592b
unknown <Student@5-USR-115P-04.std5.local>
2024-02-09 10:28:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine()) ;
+            Console.WriteLine(a) ;
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(b) ;
+            if (a > b)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("число 1 > число 2");
+                Console.ReadKey();
+            }
+            else if (a < b)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(("число 1 < число 2"));
+                Console.ReadKey();
+            }
+            else if (a == b)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(("число 1 = число 2"));
+                Console.ReadKey();
+            }
+
+        }
+
+
+        }
+    }
+
